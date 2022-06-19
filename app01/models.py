1409d76f1f93b53dfd0003056804406438ac0c04
HuangYun <hy020316@126.com>
2022-06-19 17:02:17 +0800

    数据库功能修改

--- app01/models.py
+++ app01/models.py
@@ -44,4 +44,4 @@ class PrettyNum(models.Model):
 
 class Admin(models.Model):
     username = models.CharField(verbose_name='用户名', max_length=32)
-    passowrd = models.CharField(verbose_name='密码', max_length=64)
+    password = models.CharField(verbose_name='密码', max_length=64)
--- app01/utils/form.py
+++ app01/utils/form.py
@@ -59,15 +59,15 @@ class AdminModelForm(BootStrapModelForm):
         model = models.Admin
         fields = '__all__'
         widgets = {
-            'passowrd': forms.PasswordInput(render_value=True),
+            'password': forms.PasswordInput(render_value=True),
         }
 
-    def clean_passowrd(self):
-        pwd = self.cleaned_data['passowrd']
+    def clean_password(self):
+        pwd = self.cleaned_data['password']
         return md5(pwd)
 
     def clean_confirm_password(self):
-        pwd = self.cleaned_data['passowrd']
+        pwd = self.cleaned_data['password']
         confirm = md5(self.cleaned_data['confirm_password'])
         if pwd != confirm:
             raise ValidationError('两次输入的密码不一致！')
