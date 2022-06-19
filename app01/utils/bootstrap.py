8ecc3bed9477a26931259c551152470da856e799
HuangYun <hy020316@126.com>
2022-06-19 13:32:47 +0800

    BootStrap组件继承

---
+++ app01/utils/bootstrap.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class BootStrapModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 循环找到所有插件 ，赋予样式
+        for name, field in self.fields.items():
+            if field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
+                field.widget.attrs['placeholder'] = field.label
+            else:
+                field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
--- app01/utils/form.py
+++ app01/utils/form.py
@@ -2,9 +2,9 @@ from django import forms
 from app01 import models
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from app01.utils.bootstrap import *
 
-
-class UserModelForm(forms.ModelForm):
+class UserModelForm(BootStrapModelForm):
     class Meta:
         model = models.UserInfo
         fields = ['name', 'password', 'age', 'account', 'create_time', 'depart', 'gender']
@@ -20,18 +20,12 @@ class UserModelForm(forms.ModelForm):
 
         widgets = {
             #     'name' : forms.TextInput(attrs={'class':'form-control'}),
-             'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            #  'password': forms.PasswordInput(attrs={'class': 'form-control'}),
             #     'age'
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 循环找到所有插件 ，赋予样式
-        for name, field in self.fields.items():
-            field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
 
-
-class PrettyModelForm(forms.ModelForm):
+class PrettyModelForm(BootStrapModelForm):
     mobile = forms.CharField(
         label='手机号',
         validators=[RegexValidator(r'^1[3-9]\d{9}$', '手机号格式错误'), ],
@@ -42,12 +36,6 @@ class PrettyModelForm(forms.ModelForm):
         # fields = ['mobile', 'price', 'level', 'status']
         fields = '__all__'
         # exclude = ['']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 循环找到所有插件 ，赋予样式
-        for name, field in self.fields.items():
-            field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
 
     def clean_mobile(self):
         txt_mobile = self.cleaned_data['mobile']
